e13af0eb94f67310baeec6fb9531b434a78cb40d
SlawekLysakowski <slawku1@gmail.com>
2024-02-01 17:48:25 +0100

    Set up for heroku deployment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://slaweksvocabtool.com/api/'
-  // apiUrl: 'http://localhost:3000/api/',
-  // apiUrl: 'https://slaweksvocabtool-854b2d498631.herokuapp.com/api/',
+  apiUrl: 'https://slaweksvocab-9d1fda502f52.herokuapp.com/api/'
+  // apiUrl: 'http://slaweksvocabtool.com/api/'
+  // apiUrl: 'http://localhost:3000/api/'
 };
